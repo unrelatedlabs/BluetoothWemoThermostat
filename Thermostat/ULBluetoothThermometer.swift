a71f01033e235224a78772d225a019c3e18e5633
Peter Kuhar <peter@azumio.com>
2015-01-23 08:27:55 -0800

    first commit

---
+++ Thermostat/ULBluetoothThermometer.swift
@@ -0,0 +1,13 @@
+//
+//  ULBluetoothThermometer.swift
+//  Thermostat
+//
+//  Created by Peter Kuhar Jagodnik on 1/22/15.
+//  Copyright (c) 2015 Peter Kuhar. All rights reserved.
+//
+
+import UIKit
+
+class ULBluetoothThermometer: NSObject {
+   
+}
